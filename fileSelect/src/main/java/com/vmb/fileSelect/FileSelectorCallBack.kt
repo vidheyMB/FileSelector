104c987181ba040d53837ad288b5cf5ffe508fa7
vidhey <manoj@in.arokiait.com>
2020-12-19 20:16:13 +0530

    Initial commit

--- fileSelect/src/main/java/com/vmb/fileSelect/FileSelectorCallBack.kt
+++ fileSelect/src/main/java/com/vmb/fileSelect/FileSelectorCallBack.kt
@@ -1,8 +1,9 @@
 package com.vmb.fileSelect
 
+import android.os.Parcelable
 import java.io.Serializable
 
 /** CallBack to the activity after data fetch*/
-interface FileSelectorCallBack : Serializable{
+interface FileSelectorCallBack : Parcelable{
     fun onResponse(response: String, extension: String)
 }
